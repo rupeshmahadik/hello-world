43e02024ca00e57d9d86f08435fbc688512a2aca
rupesh <rupeshmahadik38@gmail.com>
2020-12-12 21:51:22 +0530

    first commit added few things

---
+++ main.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('btn')
+const nav = document.getElementById('nav')
+const login_form = document.getElementById('login_form')
+const login_popup = document.getElementById('login_popup')
+const signup_form = document.getElementById('signup_form')
+const signup_popup = document.getElementById('signup_popup')
+
+btn.addEventListener('click',()=>{
+	nav.classList.toggle('active')
+	btn.classList.toggle('active')
+})
+
+
+login_form.addEventListener('click', ()=>{
+	login_popup.classList.toggle('active')
+	login_form.classList.toggle('active')
+})
+
+signup_form.addEventListener('click', ()=>{
+	signup_popup.classList.toggle('active')
+	signup_form.classList.toggle('active')
+})
