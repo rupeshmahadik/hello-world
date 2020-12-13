5a33330182fea56fc5ed85f5a0434823dd8cf80c
rupesh <rupeshmahadik38@gmail.com>
2020-12-13 22:05:25 +0530

    fixed login/signup popup issues

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const btn = document.getElementById('btn')
 const nav = document.getElementById('nav')
 const login_form = document.getElementById('login_form')
 const login_popup = document.getElementById('login_popup')
+const login_close = document.getElementById('login_close')
 const signup_form = document.getElementById('signup_form')
 const signup_popup = document.getElementById('signup_popup')
+const signup_close = document.getElementById('signup_close')
 
 btn.addEventListener('click',()=>{
 	nav.classList.toggle('active')
@@ -16,7 +18,15 @@ login_form.addEventListener('click', ()=>{
 	login_form.classList.toggle('active')
 })
 
+login_close.addEventListener('click',()=>{
+	login_popup.classList.toggle('active')
+})
+
 signup_form.addEventListener('click', ()=>{
 	signup_popup.classList.toggle('active')
 	signup_form.classList.toggle('active')
-})
+})  
+
+signup_close.addEventListener('click',()=>{
+	signup_popup.classList.toggle('active')
+}) 
